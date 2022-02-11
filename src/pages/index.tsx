2ee9d17ca9f63c739206ddcce38fdb059a019331
Takashi Makimoto <mackie@beehive-dev.com>
2022-02-11 06:47:29 +0000

    gatsby-plugin-react-helmet プラグインを追加

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 
 const IndexPage: React.FC = () => (
-  <div>
-    <div>Hello World</div>
-  </div>
+  <>
+    <Helmet
+      htmlAttributes={{ lang: 'ja' }}
+      title="gatsby-typescript-starter"
+      meta={[{ name: 'description', content: 'gatsby typescript starter' }]}
+    />
+    <div>
+      <div>Hello World</div>
+    </div>
+  </>
 );
 
 export default IndexPage;

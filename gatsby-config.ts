0cb77c5191332dd4586a21b3dedb5573a5813f10
Takashi Makimoto <mackie@beehive-dev.com>
2022-02-11 06:38:27 +0000

    gatsby-plugin-manifest プラグインを追加

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -11,6 +11,18 @@ const config: GatsbyConfig = {
   pathPrefix: process.env.PATH_PREFIX || '',
   plugins: [
     {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: 'gatsby-typescript-starter',
+        short_name: 'gatsby-typescript-starter',
+        start_url: '/',
+        background_color: '#fff',
+        theme_color: '#000',
+        display: 'minimal-ui',
+        icon: path.resolve(__dirname, 'src', 'images', 'icon.png'),
+      },
+    },
+    {
       resolve: 'gatsby-plugin-typegen',
       options: {
         outputPath: path.resolve(__dirname, 'src', '@types', 'gatsby-types.d.ts'),
